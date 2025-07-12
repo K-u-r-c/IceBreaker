355b8d11cb188aa4165dcd0f3042f2ece2ba846a
Jakub <58849519+K-u-r-c@users.noreply.github.com>
2025-07-12 12:28:57 +0200

    Refactor ice_breaker script to use summary_parser for output and add output_parsers module for structured summary data

--- ice_breaker.py
+++ ice_breaker.py
@@ -5,26 +5,30 @@ from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_patries.linkedin import scrape_linkedin_profile
+from output_parsers import summary_parser
 
 
 def ice_break_with(name: str) -> str:
     linkedin_url = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url, mock=True)
     
     summary_template = """
         Given the {information} about a person from I want you to create:
         1. a short summary
         2. two interesting facts about them
+        \n{format_instructions}
     """
 
     summpary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"], 
+        template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()}
     )
 
     llm = ChatOpenAI(temperature=0, model="gpt-4o-mini")
     # llm = ChatOllama(temperature=0, model="llama3")
     
-    chain = summpary_prompt_template | llm | StrOutputParser()
+    chain = summpary_prompt_template | llm | summary_parser
 
     res = chain.invoke(input={"information": linkedin_data})
 
